e5db14e20090270ab02a51fe0e3078e00989c250
calvin howard <calvin.uhh@gmail.com>
2024-07-14 11:09:28 -0500

    Agregando efectos css a la vista de Lista de los componentes

--- client/src/components/Cards/GridView.jsx
+++ client/src/components/Cards/GridView.jsx
@@ -3,14 +3,27 @@ import style from "./gridView.module.css";
 const GridView = ({ data }) => {
   return (
     <div id={style.main_container}>
-      <table>
-        <thead>
-          <tr>
-            <th>hola</th>
-            <th>hola</th>
-            <th></th>
+      <table id={style.table}>
+        <thead id={style.head}>
+          <tr id={style.table_row}>
+            <th className={style.table_head}>Volumen</th>
+            <th className={style.table_head}>Nombre</th>
+            <th className={style.table_head}>Fecha de salida</th>
+            <th className={style.table_head}>Numero de Comic</th>
           </tr>
         </thead>
+        <tbody>
+          {data.map((elem) => (
+            <tr key={elem.id}>
+              <td className={`${style.table_data} ${style.volume}`}>
+                {elem.volume}
+              </td>
+              <td className={style.table_data}>{elem.name}</td>
+              <td className={style.table_data}>{elem.date}</td>
+              <td className={style.table_data}>{elem.issue_number}</td>
+            </tr>
+          ))}
+        </tbody>
       </table>
     </div>
   );
--- client/src/components/Cards/ListView.jsx
+++ client/src/components/Cards/ListView.jsx
@@ -1,15 +1,44 @@
+import { useRef, useEffect } from "react";
+
 import style from "./listView.module.css";
 
 const ListView = ({ data }) => {
+  const containersRef = useRef([]);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add(style.fade_in_visible);
+            observer.unobserve(entry.target);
+          }
+        });
+      },
+      { threshold: 0.1 }
+    );
+
+    containersRef.current.forEach((container) => {
+      observer.observe(container);
+    });
+
+    return () => observer.disconnect();
+  }, []);
+
   return (
     <div id={style.main_container}>
-      {data.map((elem) => (
-        <div className={style.container} key={elem.id}>
+      {data.map((elem, index) => (
+        <div
+          ref={(el) => (containersRef.current[index] = el)}
+          className={`${style.container} ${style.fade_in}`}
+          key={elem.id}
+        >
           <img
             className={style.image}
             src={elem.image}
             alt={`${elem.volume} image`}
           />
+          <div className={style.image_overlay}>Ver detalles</div>
           <h1 className={style.volume}>{elem.volume}</h1>
           <h2 className={style.name}>
             {elem.name ? `Nombre: ${elem.name}` : null}
