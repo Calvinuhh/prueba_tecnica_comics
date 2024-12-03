822f159d2c011de16496c85bfddb094198eb71ca
Calvin Ulis Howard Howard <140646387+Calvinuhh@users.noreply.github.com>
2024-12-02 23:55:14 -0500

    Actualizar db.js

--- server/src/database/db.js
+++ server/src/database/db.js
@@ -2,7 +2,6 @@ import { Sequelize } from "sequelize";
 
 import IssueModel from "../models/Issue.js";
 
-process.loadEnvFile();
 const { DB_USER, DB_PASSWORD, DB_HOST, DATABASE_NAME } = process.env;
 
 const database = new Sequelize(
